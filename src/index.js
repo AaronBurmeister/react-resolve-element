693e26f6e1b80d978cae2c8d284d35873165e932
Aaron Burmeister <aaron.burmeister@ils-gmbh.net>
2017-07-18 12:17:51 +0200

    added source

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function resolveElement(
+  { component, render, children } = {}, // eslint-disable-line react/prop-types
+  props = {},
+  defaultValue = null,
+) {
+  if (component) return (<component {...props} />)
+  if (render) return (render(props))
+  if (children) {
+    if (typeof children === 'function') return (children(props))
+    if (!Array.isArray(children) || children.length) return (React.Children.only(children))
+  }
+
+  return defaultValue
+}
--- test/test.js
+++ test/test.js
@@ -1,1 +1,65 @@
+/* eslint-disable no-unused-expressions */
+
+import React from 'react'
 import { expect } from 'chai'
+
+import resolveElement from '../'
+
+describe('react-resolve-element', () => {
+  it('should export the resolveElement function', done => {
+    expect(resolveElement).to.exist
+    expect(resolveElement).to.be.a('function')
+    done()
+  })
+
+  describe('resolveElement()', () => {
+    describe('with component prop', () => {
+      it('should return a React component if component is provided', done => {
+        expect(resolveElement({ component: () => null })).to.exist
+        done()
+      })
+
+      it('should return the component even if render or children is provided', done => {
+        const children = (<span>React text 2</span>)
+        const result = resolveElement(
+          { component: () => null, render: () => 'React text', children }
+        )
+        expect(result).to.exist
+        expect(result).to.not.equal('React text')
+        expect(result).to.not.equal(children)
+        done()
+      })
+    })
+
+    describe('with render prop', () => {
+      it('should return the result of render() if render is provided', done => {
+        expect(resolveElement({ render: () => 'React text' })).to.equal('React text')
+        done()
+      })
+
+      it('should return the result of render() even if children is provided', done => {
+        const children = (<span>React text 2</span>)
+        expect(resolveElement({ render: () => 'React text', children })).to.equal('React text')
+        done()
+      })
+    })
+
+    describe('with children prop', () => {
+      it('should return the children if provided', done => {
+        const children = (<span>React text 2</span>)
+        expect(resolveElement({ children })).to.equal(children)
+        done()
+      })
+    })
+
+    it('returns the defaultProp if neither component, render nor children are provided', done => {
+      expect(resolveElement({}, {}, 'default')).to.equal('default')
+      done()
+    })
+
+    it('accepts undefined props', done => {
+      expect(resolveElement).to.not.throw
+      done()
+    })
+  })
+})
